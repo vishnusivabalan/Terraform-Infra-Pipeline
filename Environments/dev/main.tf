4ac7204817ae65078c585caa56a4c21df4c49fa6
vishnu <vishnusivabalan.sp@gmail.com>
2025-08-03 18:17:49 +0800

    modified dev name

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source           = "../../Modules/vpc"
-  Env              = "Dev"
+  Env              = "dev"
   vpc_cidr         = "10.0.0.0/16"
   pubSubnet_cidr   = "10.0.0.1/24"
   pvtSubnet_cidr   = "10.0.0.2/24"
@@ -13,5 +13,5 @@ module "ec2" {
   instance_type = "t3.micro"
   subnet_id     = module.vpc.subnet_id
   ec2_count     = "1"
-  Env           = "Dev"
+  Env           = "dev"
 }
