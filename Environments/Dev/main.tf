3874124d91ea261c6f5c593501cf2144295d0f5c
vishnu <vishnusivabalan.sp@gmail.com>
2025-08-03 16:38:26 +0800

    files added tp QA amd Prod

---
+++ Environments/Dev/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source           = "../../Modules/vpc"
+  Env              = "Dev"
+  vpc_cidr         = "10.0.0.0/16"
+  pubSubnet_cidr   = "10.0.0.1/24"
+  pvtSubnet_cidr   = "10.0.0.2/24"
+  subnet_aval_Zone = "${var.aws_region_name}a"
+}
+
+module "ec2" {
+  source        = "../../Modules/ec2"
+  ami_id        = "ami-015927f8ee1bc0293"
+  instance_type = "t3.micro"
+  subnet_id     = module.vpc.subnet_id
+  ec2_count     = "1"
+  Env           = "Dev"
+}
